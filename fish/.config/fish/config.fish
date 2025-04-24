51d615f732325f247618c4d7593aff84a3e10c6e
Francesco Lucarelli <francesco06lucarelli@gmail.com>
2025-04-24 21:26:33 +0200

    update config.fish : 2025-04-24 21:26:33

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -40,8 +40,6 @@ end
 if type -q starship
     starship init fish | source
 end
-# Attiva i suggerimenti
-source (status --is-interactive; and echo $fish_function_path[1]/fish_autosuggestions.fish)
 
 # ========================
 # Functions
